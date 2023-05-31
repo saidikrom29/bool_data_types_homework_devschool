16128a5252821f1c33953d30e67ce8ddc8da501f
saidikrom29 <saidikrom2901>
2023-06-01 01:20:59 +0500

    bool5

--- bool5.dart
+++ bool5.dart
@@ -1,5 +1,13 @@
-  // Create function named fun
-//Create variable int a  
+// Create function named fun
+//Create variable int a
 //Check the logic "The variable "a" is an odd number"
 //     Returns:
 //         bool
+bool fun(int a) {
+  bool x;
+  x = a%2==1;
+  return (x);
+}
+void main() {
+  print(fun(3));
+}
