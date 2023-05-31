25a5fa8b2919e83159b96e8a45cf916cd2bc51ce
saidikrom29 <saidikrom2901>
2023-06-01 02:17:06 +0500

    bool7

--- bool7.dart
+++ bool7.dart
@@ -1,5 +1,14 @@
-      // Create function named fun
-    //Create variable int a,int b
-    // Check the logic "They are not equal"
-    // Returns:
-    //     bool
+// Create function named fun
+//Create variable int a,int b
+// Check the logic "They are not equal"
+// Returns:
+//     bool
+bool func(int a, int b) {
+  bool x;
+  x = a != b;
+  return x;
+}
+
+void main() {
+  print(func(5, 5));
+}
