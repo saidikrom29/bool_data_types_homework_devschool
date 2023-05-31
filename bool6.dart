cb9e61d56173472888ccc6402b6357972ee3685e
saidikrom29 <saidikrom2901>
2023-06-01 02:12:10 +0500

    bool6

--- bool6.dart
+++ bool6.dart
@@ -1,5 +1,15 @@
-    // Create function named fun
-  //Create variable int a 
-  //Check the logic "The variable "a" is an even number"
-  //   Returns:
-  //       bool
+// Create function named fun
+//Create variable int a
+//Check the logic "The variable "a" is an even number"
+//   Returns:
+//       bool
+bool func(int a) {
+  bool x;
+  int son = a % 2;
+  x = son == 0; 
+  return x;
+}
+
+void main() {
+  print(func(6));
+}
