cb6a69547c64de9bbc9aac9f032108feb34f9ade
saidikrom29 <saidikrom2901>
2023-05-31 23:55:12 +0500

    bool2

--- bool2.dart
+++ bool2.dart
@@ -1,6 +1,15 @@
-  // Create function named fun
+// Create function named fun
 //Create a variable int a
 //  Check the logic "The variable 'a' is equal 7"
 
 //     Returns:
 //         bool
+bool func(int A) {
+  bool x;
+  x = 7 >= 7;
+  return (x);
+}
+
+void main() {
+  print(func(7));
+}
