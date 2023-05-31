d60ed7ea24105230a75a27d528cf75b9d02268d6
saidikrom29 <saidikrom2901>
2023-06-01 00:10:13 +0500

    bool4

--- bool4.dart
+++ bool4.dart
@@ -1,7 +1,17 @@
-  // Create function named fun
+// Create function named fun
 //Create variable   inty a
 //Check the logic "The variable "a" is negative"
 
 //     Returns:
 //         bool
 //     """
+bool func(int a, int b) {
+  bool x;
+  x = 900 <= 1111111;
+  return (x);
+}
+
+void main() {
+  print(func(900, 11111111));
+
+}
